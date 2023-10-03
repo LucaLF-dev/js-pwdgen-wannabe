b653077ffe39384e487a160b711dcdf95f3ce7b9
LucaLF-dev <laficaraluca@gmail.com>
2023-10-03 15:41:33 +0200

    Add - first variable (name) js

---
+++ js/generator.js
@@ -0,0 +1,25 @@
+// # (insicurisimo) Password generator
+
+// - Chiediere all’utente il suo nome:
+//   - dichiarare una variabile 
+let userName
+//   - assegnare alla variabile il valore restituito dal prompt sul nome
+userName = prompt('Ciao, qual è il tuo nome?')
+console.log(userName)
+
+// - Chiedere il suo cognome:
+//   - dichiarare una variabile 
+//   - assegnare alla variabile il valore restituito dal prompt sul cognome
+// - Chiedere il suo colore preferito:
+//   - dichiarare una variabile 
+//   - assegnare alla variabile il valore restituito dal prompt sul colore
+// - Concatenare il nome , il cognome, il colore preferito dell' utente e il numero 21:
+//   - dichiarare una variabile cho contiene la prima parte della password
+//   - dichiarare una variabile cho contiene la seconda parte della password
+//   - dichiarare una variabile cho contiene la terza parte della password
+//   - dichiarare una variabile cho contiene la quarta parte della password
+//   - dichiarare una variabile password dove concateniamo le tre stringhe ed il numero 21
+// - Scrivere sulla pagina il risultato della concatenazione: nomecognomecolorepreferito21
+//   - inserire nell' html un tag con id="password"
+//   - recuperare con js l' elemento del Dom con id="password"
+//   - modificare l ' innerHTML del tag con il valore della variabile password 
